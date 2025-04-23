77ee028295c2a8e58e1570f58bea8b1a8bbaab83
NicksEdits <nicolasplanche972@gmail.com>
2025-04-23 15:41:56 +0200

    Image to img

--- src/pages/favicon.ico.ts
+++ src/pages/favicon.ico.ts
@@ -1,26 +1,10 @@
 import type { APIRoute } from "astro";
-import sharp from "sharp";
-import ico from "sharp-ico";
+import fs from "node:fs/promises";
 import path from "node:path";
 
-const faviconSrc = path.resolve("src/images/icon.png");
-
 export const GET: APIRoute = async () => {
-
-  // Resize the image to multiple sizes
-  const sizes = [16, 32];
-
-  const buffers = await Promise.all(
-    sizes.map(async (size) => {
-      return await sharp(faviconSrc)
-        .resize(size)
-        .toFormat("png")
-        .toBuffer();
-    })
-  );
-
-  // Convert the image to an ICO file
-  const icoBuffer = ico.encode(buffers);
+  const icoPath = path.resolve("src/images/icon.png");
+  const icoBuffer = await fs.readFile(icoPath);
 
   return new Response(icoBuffer, {
     headers: { "Content-Type": "image/x-icon" },
